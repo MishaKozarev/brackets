269851b351b737e609aec5030806284246e2fcc0
MishaKozarev <mishaaaaa1986@mail.ru>
2023-01-29 21:19:00 +0300

    feat: modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+let arrStr = []
+let arrArr = []
+let arrAll = []
+let stack = [];
+//Создаем обьект - ключ(закрывающая):значение(открывающая)
+let brackets = { [')'] : '(', ['}'] : '{', [']'] : '[', ['|'] : '|', ['2'] : '1', ['4'] : '3', ['6'] : '5', ['7'] : '7', ['8'] : '8'}
+
+//Входной массив - убираем подмассивы и обьеденяем все в один массив
+arrArr = bracketsConfig.flat()
+//Входная строка - разбиваем строку на элементы и ложим в массив
+arrStr = str.split('')
+//Обьеденяем все входные данные в один массив
+arrAll = [...arrStr, ...arrArr]
+
+    for(let i = 0; i < arrAll.length; i++) {
+//Элемент массива
+    let iarrAll = arrAll[i];
+//Верхний элемент в стэке
+    let topElStack = stack[stack.length - 1];
+      if(brackets[iarrAll] === topElStack && stack.length !== 0) {
+        stack.pop();
+      } else {
+        stack.push(iarrAll)
+        }
 }
+//Если в стэке что-то есть -false, если ничего нет -true
+  return stack.length === 0;
+}
+
+
+
+
+
+
